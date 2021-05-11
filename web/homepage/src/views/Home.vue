abb06332040839ce4648eab0245f4ae1f2028705
willie.jiao <dongrun.jiao@carota.ai>
2021-05-11 15:31:22 +0800

    web

--- web/homepage/src/views/Home.vue
+++ web/homepage/src/views/Home.vue
@@ -1,133 +1,417 @@
 <template>
   <div class="maincontent">
-      <div class="bili-banner" style="">
-        <div class="animated-banner">
-          <div class="layer">
-            <img src="../assets/img/c12c6ceaa5f78de17cd94bb0ad325339b2386ee8.png" data-height="360" data-width="9666" height="231" width="6204" style="transform: scale(1) translate(0px, -19.2581px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/4d5ddb371f6d710cf0222a5d0cef46ea3ed5901c.png" data-height="360" data-width="9666" height="231" width="6204" style="transform: scale(1) translate(1412.26px, 0px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/c7a1dbf9bbc09c1f867b14a141c4e2f07c4b228d.png" data-height="360" data-width="3523" height="207" width="2035" style="transform: scale(1) translate(866.613px, 0px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/d605a7076d5fe89d0d7e3d6219bffe4000033a09.png" data-height="360" data-width="2938" height="226" width="1848" style="transform: scale(1) translate(-817.826px, 0px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/8d947f22c6a67b8a5d2fbe1424ca2a946802e729.png" data-height="139" data-width="556" height="80" width="321" style="transform: scale(1) translate(779.952px, 57.7742px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/65a860abda015ed247d48b1bcc5cbb9ce725d050.png" data-height="302" data-width="734" height="108" width="263" style="transform: scale(1) translate(323.535px, 46.7329px) rotate(0deg); opacity: 0;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/963685e7d82d27ba5e8187680814a2d548b5adbe.png" data-height="180" data-width="1757" height="161" width="1579" style="transform: scale(1) translate(143.794px, 17.9742px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/6a6e6e52a18a7731645aa1d93b1d74f2f628f77a.png" data-height="116" data-width="1757" height="104" width="1579" style="transform: scale(1) translate(-449.355px, 62.9097px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/3969636231e7668ec91985bb147c89a6b69021c7.png" data-height="346" data-width="497" height="177" width="255" style="transform: scale(1) translate(-308.129px, 20.5419px) rotate(0deg); opacity: 0;">
-          </div>
-          <div class="layer">
-            <!-- <img src="../assets/img/9af4cdc3c81e01522fc4df82a5b7cfa8b03b6b5b.png" data-height="256" data-width="146" height="131" width="74" style="transform: scale(1) translate(-436.516px, 41.0839px) rotate(0deg); opacity: 0;"> -->
-          </div>
-          <div class="layer">
-            <img src="../assets/img/4d8514308bd6673b215cdb1aadbf12891d784672.png" data-height="254" data-width="602" height="146" width="347" style="transform: scale(1) translate(-115.548px, 17.3323px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/9b4f18f9f18c8dca17ba6c0b46a1dcedb649c82b.png" data-height="360" data-width="4277" height="231" width="2745" style="transform: scale(1) translate(128.387px, 0px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/b24c872ae1ef34ebe794d1631a50b57fbdb0a482.png" data-height="327" data-width="933" height="188" width="539" style="transform: scale(1) translate(277.316px, 17.3323px) rotate(0deg); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/45031fa60ea6084886e80ca9e906803f9a3221b1.png" data-height="353" data-width="740" height="271" width="570" style="transform: scale(1) translate(2696.13px, 0px) rotate(0deg); filter: blur(2px); opacity: 1;">
-          </div>
-          <div class="layer">
-            <img src="../assets/img/17a00335345ecb9f5439069ee2d069772a9cede3.png" data-height="360" data-width="1916" height="231" width="1229" style="transform: scale(1) translate(-1124.87px, 0px) rotate(0deg); filter: blur(1px); opacity: 1;">
-          </div>
-          <canvas width="2121" height="198" style="position: absolute; top: 0px; left: 0px;"></canvas>
+    <div class="bili-banner" style="">
+      <div class="animated-banner">
+        <div class="layer">
+          <img
+            src="../assets/img/c12c6ceaa5f78de17cd94bb0ad325339b2386ee8.png"
+            data-height="360"
+            data-width="9666"
+            height="231"
+            width="6204"
+            style="
+              transform: scale(1) translate(0px, -19.2581px) rotate(0deg);
+              opacity: 1;
+            "
+          />
         </div>
-        <div class="taper-line" data-v-cd8268ae=""></div>
-        <div class="b-logo b-wrap" style="margin: 0" data-v-cd8268ae="">
-          <a href="https://www.bilibili.com/" class="head-logo" data-v-cd8268ae="">
-            <!-- <img src="../assets/img/c1e19150b5d1e413958d45e0e62f012e3ee200af.png" class="logo-img" data-v-cd8268ae=""> -->
-          </a>
-          <!---->
+        <div class="layer">
+          <img
+            src="../assets/img/4d5ddb371f6d710cf0222a5d0cef46ea3ed5901c.png"
+            data-height="360"
+            data-width="9666"
+            height="231"
+            width="6204"
+            style="
+              transform: scale(1) translate(1412.26px, 0px) rotate(0deg);
+              opacity: 1;
+            "
+          />
         </div>
+        <div class="layer">
+          <img
+            src="../assets/img/c7a1dbf9bbc09c1f867b14a141c4e2f07c4b228d.png"
+            data-height="360"
+            data-width="3523"
+            height="207"
+            width="2035"
+            style="
+              transform: scale(1) translate(866.613px, 0px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/d605a7076d5fe89d0d7e3d6219bffe4000033a09.png"
+            data-height="360"
+            data-width="2938"
+            height="226"
+            width="1848"
+            style="
+              transform: scale(1) translate(-817.826px, 0px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/8d947f22c6a67b8a5d2fbe1424ca2a946802e729.png"
+            data-height="139"
+            data-width="556"
+            height="80"
+            width="321"
+            style="
+              transform: scale(1) translate(779.952px, 57.7742px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/65a860abda015ed247d48b1bcc5cbb9ce725d050.png"
+            data-height="302"
+            data-width="734"
+            height="108"
+            width="263"
+            style="
+              transform: scale(1) translate(323.535px, 46.7329px) rotate(0deg);
+              opacity: 0;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/963685e7d82d27ba5e8187680814a2d548b5adbe.png"
+            data-height="180"
+            data-width="1757"
+            height="161"
+            width="1579"
+            style="
+              transform: scale(1) translate(143.794px, 17.9742px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/6a6e6e52a18a7731645aa1d93b1d74f2f628f77a.png"
+            data-height="116"
+            data-width="1757"
+            height="104"
+            width="1579"
+            style="
+              transform: scale(1) translate(-449.355px, 62.9097px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/3969636231e7668ec91985bb147c89a6b69021c7.png"
+            data-height="346"
+            data-width="497"
+            height="177"
+            width="255"
+            style="
+              transform: scale(1) translate(-308.129px, 20.5419px) rotate(0deg);
+              opacity: 0;
+            "
+          />
+        </div>
+        <div class="layer">
+          <!-- <img src="../assets/img/9af4cdc3c81e01522fc4df82a5b7cfa8b03b6b5b.png" data-height="256" data-width="146" height="131" width="74" style="transform: scale(1) translate(-436.516px, 41.0839px) rotate(0deg); opacity: 0;"> -->
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/4d8514308bd6673b215cdb1aadbf12891d784672.png"
+            data-height="254"
+            data-width="602"
+            height="146"
+            width="347"
+            style="
+              transform: scale(1) translate(-115.548px, 17.3323px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/9b4f18f9f18c8dca17ba6c0b46a1dcedb649c82b.png"
+            data-height="360"
+            data-width="4277"
+            height="231"
+            width="2745"
+            style="
+              transform: scale(1) translate(128.387px, 0px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/b24c872ae1ef34ebe794d1631a50b57fbdb0a482.png"
+            data-height="327"
+            data-width="933"
+            height="188"
+            width="539"
+            style="
+              transform: scale(1) translate(277.316px, 17.3323px) rotate(0deg);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/45031fa60ea6084886e80ca9e906803f9a3221b1.png"
+            data-height="353"
+            data-width="740"
+            height="271"
+            width="570"
+            style="
+              transform: scale(1) translate(2696.13px, 0px) rotate(0deg);
+              filter: blur(2px);
+              opacity: 1;
+            "
+          />
+        </div>
+        <div class="layer">
+          <img
+            src="../assets/img/17a00335345ecb9f5439069ee2d069772a9cede3.png"
+            data-height="360"
+            data-width="1916"
+            height="231"
+            width="1229"
+            style="
+              transform: scale(1) translate(-1124.87px, 0px) rotate(0deg);
+              filter: blur(1px);
+              opacity: 1;
+            "
+          />
+        </div>
+        <canvas
+          width="2121"
+          height="198"
+          style="position: absolute; top: 0px; left: 0px"
+        ></canvas>
+      </div>
+      <div class="taper-line" data-v-cd8268ae=""></div>
+      <div class="b-logo b-wrap" style="margin: 0" data-v-cd8268ae="">
+        <a
+          href="https://www.bilibili.com/"
+          class="head-logo"
+          data-v-cd8268ae=""
+        >
+          <!-- <img src="../assets/img/c1e19150b5d1e413958d45e0e62f012e3ee200af.png" class="logo-img" data-v-cd8268ae=""> -->
+        </a>
         <!---->
       </div>
+      <!---->
+    </div>
+    <div class="todo"></div>
   </div>
 </template>
 
 <script lang="ts">
 //import "./test.css";
-import { ref, defineComponent } from 'vue'
+import Todo from "./Todo.vue";
+import { ref, defineComponent } from "vue";
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+    const count = ref(0);
+    return { count };
+  },
+});
 </script>
 <style>
-body{margin:0;padding:0;color:#505050;font:12px -apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,sans-serif;-webkit-font-smoothing:antialiased}
+body {
+  margin: 0;
+  padding: 0;
+  color: #505050;
+  font: 12px -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial,
+    PingFang SC, Hiragino Sans GB, Microsoft YaHei, sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
 .bili-banner {
-  margin:0 auto;position:relative;z-index:0;min-height:155px;
-  height:9.375vw;min-width:999px;background-color:#f9f9f9;
-  display:-ms-flexbox;display:flex;
-  -ms-flex-pack:center;justify-content:center;
-  background-repeat:no-repeat;
-  background-position:center 0;
-  background-size:cover
+  margin: 0 auto;
+  position: relative;
+  z-index: 0;
+  min-height: 155px;
+  height: 9.375vw;
+  min-width: 999px;
+  background-color: #f9f9f9;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-pack: center;
+  justify-content: center;
+  background-repeat: no-repeat;
+  background-position: center 0;
+  background-size: cover;
 }
-.bili-banner .taper-line{
-  pointer-events:none;
-  position:absolute;top:0;left:0;
-  z-index:0;width:100%;height:100px;
-  background:linear-gradient(rgba(0,0,0,.4),transparent)
+.bili-banner .taper-line {
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 100%;
+  height: 100px;
+  background: linear-gradient(rgba(0, 0, 0, 0.4), transparent);
 }
-.bili-banner .head-title{
-  position:absolute;bottom:25px;left:280px;
-  line-height:20px;padding:6px 10px;
-  background-color:rgba(0,0,0,.68);color:#fff;border-radius:4px;
-  font-size:14px;max-width:350px;opacity:0;transition:all .2s
+.bili-banner .head-title {
+  position: absolute;
+  bottom: 25px;
+  left: 280px;
+  line-height: 20px;
+  padding: 6px 10px;
+  background-color: rgba(0, 0, 0, 0.68);
+  color: #fff;
+  border-radius: 4px;
+  font-size: 14px;
+  max-width: 350px;
+  opacity: 0;
+  transition: all 0.2s;
 }
-.bili-banner:hover .head-title{
-  opacity:1
+.bili-banner:hover .head-title {
+  opacity: 1;
 }
-.bili-banner .b-logo{
-  position:relative
+.bili-banner .b-logo {
+  position: relative;
 }
-.bili-banner .b-logo .head-logo{
-  position:absolute;
-  width:220px;height:50%;
-  min-height:60px;bottom:10px;z-index:1
+.bili-banner .b-logo .head-logo {
+  position: absolute;
+  width: 220px;
+  height: 50%;
+  min-height: 60px;
+  bottom: 10px;
+  z-index: 1;
 }
-.bili-banner .b-logo .logo-img{
-  height:100%
+.bili-banner .b-logo .logo-img {
+  height: 100%;
 }
-.bili-banner .banner-link{
-  z-index:0;
-  position:absolute;left:0;top:0;
-  height:100%;width:100%;
+.bili-banner .banner-link {
+  z-index: 0;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
 }
-.animated-banner{
-  position:absolute;
-  top:0;bottom:0;left:0;right:0;
-  overflow:hidden;
+.animated-banner {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+}
+.animated-banner > .layer {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+.bili-banner .b-logo .logo-img {
+  height: 100%;
+}
+.bili-banner .banner-link {
+  z-index: 0;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.b-wrap {
+  width: 1630px;
+  margin: 0 auto;
+}
+@media screen and (max-width: 1870px) {
+  .b-wrap {
+    width: 1414px;
   }
-.animated-banner>.layer{
-  position:absolute;
-  left:0;top:0;
-  height:100%;width:100%;
-  display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;
-  -ms-flex-pack:center;justify-content:center
+  .b-wrap .page-tab .con li {
+    width: 58px;
+  }
+  .b-wrap .page-tab .con li .channel-name {
+    width: 58px !important;
+  }
+  .b-wrap .friendship-link {
+    width: 242px;
+  }
 }
-.bili-banner .b-logo .logo-img{height:100%}.bili-banner .banner-link{z-index:0;position:absolute;left:0;top:0;height:100%;width:100%}
-
-.b-wrap{width:1630px;margin:0 auto}@media screen and (max-width:1870px){.b-wrap{width:1414px}.b-wrap .page-tab .con li{width:58px}.b-wrap .page-tab .con li .channel-name{width:58px!important}.b-wrap .friendship-link{width:242px}}@media screen and (max-width:1654px){.b-wrap{width:1198px}.b-wrap .page-tab .con li,.b-wrap .page-tab .con li .channel-name{width:50px!important}.b-wrap .tab-line-itnl{margin:0 8px}.b-wrap .tab-line-itnl.none{margin:0 12px 0 0}.b-wrap .friendship-link{width:220px}}@media screen and (max-width:1438px){.b-wrap{width:999px}.b-wrap .channel-menu-itnl .item.hide{display:none}.b-wrap .page-tab .con li{width:50px}.b-wrap .page-tab .con li .channel-name{width:50px!important}.b-wrap .tab-line-itnl{margin:0 8px}}.international-header.fixed-top .mini-header{position:fixed;top:0;background:#fff;box-shadow:0 2px 4px 0 rgba(0,0,0,.08)}.international-header.fixed-top .mini-header .bili-icon_dingdao_xiazaiapp,.international-header.fixed-top .mini-header .bili-icon_dingdao_zhuzhan{color:#00a1d6}.international-header.fixed-top .mini-type .nav-link .nav-link-ul .nav-link-item .link,.international-header.fixed-top .mini-type .nav-user-center .user-con .item .name{color:#212121}.international-header.fixed-top .nav-link .nav-link-ul.mini .bili-icon_dingdao_xiazaiapp{color:#00a1d6}
-
+@media screen and (max-width: 1654px) {
+  .b-wrap {
+    width: 1198px;
+  }
+  .b-wrap .page-tab .con li,
+  .b-wrap .page-tab .con li .channel-name {
+    width: 50px !important;
+  }
+  .b-wrap .tab-line-itnl {
+    margin: 0 8px;
+  }
+  .b-wrap .tab-line-itnl.none {
+    margin: 0 12px 0 0;
+  }
+  .b-wrap .friendship-link {
+    width: 220px;
+  }
+}
+@media screen and (max-width: 1438px) {
+  .b-wrap {
+    width: 999px;
+  }
+  .b-wrap .channel-menu-itnl .item.hide {
+    display: none;
+  }
+  .b-wrap .page-tab .con li {
+    width: 50px;
+  }
+  .b-wrap .page-tab .con li .channel-name {
+    width: 50px !important;
+  }
+  .b-wrap .tab-line-itnl {
+    margin: 0 8px;
+  }
+}
+.international-header.fixed-top .mini-header {
+  position: fixed;
+  top: 0;
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+}
+.international-header.fixed-top .mini-header .bili-icon_dingdao_xiazaiapp,
+.international-header.fixed-top .mini-header .bili-icon_dingdao_zhuzhan {
+  color: #00a1d6;
+}
+.international-header.fixed-top
+  .mini-type
+  .nav-link
+  .nav-link-ul
+  .nav-link-item
+  .link,
+.international-header.fixed-top
+  .mini-type
+  .nav-user-center
+  .user-con
+  .item
+  .name {
+  color: #212121;
+}
+.international-header.fixed-top
+  .nav-link
+  .nav-link-ul.mini
+  .bili-icon_dingdao_xiazaiapp {
+  color: #00a1d6;
+}
 </style>
