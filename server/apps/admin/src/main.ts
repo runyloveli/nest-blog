19ab77e630fcd16050b40dd921cd6c9bdbf0ace2
willie.jiao <dongrun.jiao@carota.ai>
2021-05-13 14:21:58 +0800

    web

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AdminModule } from './admin.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+  const options = new DocumentBuilder()
+    .setTitle('Nest Blog')
+    .setDescription('The Nest-blog API description')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
+  console.log('http://localhost:3000/api-docs');
 }
 bootstrap();
